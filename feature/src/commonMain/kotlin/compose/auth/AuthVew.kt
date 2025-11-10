667a3768e46c9ab13ee1f323e334382c81371d29
PolyakovMA <polyakovma@tis-online.com>
2025-11-10 22:09:21 +0500

    Create simple backgrounds for views

--- feature/src/commonMain/kotlin/compose/auth/AuthVew.kt
+++ feature/src/commonMain/kotlin/compose/auth/AuthVew.kt
@@ -1,17 +1,15 @@
 package compose.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
-import androidx.compose.material.ModalDrawer
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import compose.auth.viewModel.models.AuthEvent
 import compose.auth.viewModel.models.AuthState
@@ -19,22 +17,54 @@ import kotlinx.coroutines.delay
 import utils.LottieAnimation
 
 @Composable
-fun AuthView(state: AuthState, eventHandler: (AuthEvent) -> Unit){
-
-    LaunchedEffect(Unit){
+fun AuthView(state: AuthState, eventHandler: (AuthEvent) -> Unit) {
+    LaunchedEffect(Unit) {
         delay(2000)
         eventHandler.invoke(AuthEvent.ClickOpenMainScreen)
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF001F54),
+                        Color(0xFF005C97),
+                        Color(0xFF00C6FF)
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
     ) {
-        if (state.showStartAnimation){
-            LottieAnimation(
-                jsonPath = "files/startAuth_Animation.json",
-                isLoop = true
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawCircle(
+                brush = Brush.radialGradient(
+                    colors = listOf(
+                        Color.White.copy(alpha = 0.25f),
+                        Color.Transparent
+                    ),
+                    center = center,
+                    radius = size.minDimension / 1.5f
+                ),
+                center = center,
+                radius = size.minDimension / 1.5f
             )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (state.showStartAnimation) {
+                LottieAnimation(
+                    jsonPath = "files/startAuth_Animation.json",
+                    isLoop = true
+                )
+            }
         }
     }
 }
--- feature/src/commonMain/kotlin/compose/main/MainView.kt
+++ feature/src/commonMain/kotlin/compose/main/MainView.kt
@@ -1,24 +1,60 @@
 package compose.main
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import compose.main.viewModel.models.MainEvent
 import compose.main.viewModel.models.MainState
 
 @Composable
-fun MainView(state: MainState, evenHandler: (MainEvent) -> Unit){
-    Column(
-        modifier = Modifier.fillMaxSize().background(Color.Blue),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun MainView(state: MainState, evenHandler: (MainEvent) -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF001F54),
+                        Color(0xFF005C97),
+                        Color(0xFF00C6FF)
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
     ) {
-        Text(text = state.test)
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawCircle(
+                brush = Brush.radialGradient(
+                    colors = listOf(
+                        Color.White.copy(alpha = 0.25f),
+                        Color.Transparent
+                    ),
+                    center = center,
+                    radius = size.minDimension / 1.5f
+                ),
+                center = center,
+                radius = size.minDimension / 1.5f
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = state.test,
+                color = Color.White
+            )
+        }
     }
 }

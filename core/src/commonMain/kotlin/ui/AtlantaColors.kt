0ff187e6fdf9914e9909aa083409e6c401d61e1b
PolyakovMA <polyakovma@tis-online.com>
2025-11-10 22:18:17 +0500

    Add object AtlantaColors for theme colors

---
+++ core/src/commonMain/kotlin/ui/AtlantaColors.kt
@@ -0,0 +1,9 @@
+package ui
+
+import androidx.compose.ui.graphics.Color
+
+object AtlantaColors {
+    val DarkBlue = Color(0xFF001F54)
+    val MediumBlue = Color(0xFF005C97)
+    val LightBlue = Color(0xFF00C6FF)
+}

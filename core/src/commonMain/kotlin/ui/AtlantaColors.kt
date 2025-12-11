860e046ff49fa2f770276bf6a44666d0d9b90c87
PolyakovMA <polyakovma@tis-online.com>
2025-12-12 02:05:09 +0500

    Add new colors and images

--- core/src/commonMain/kotlin/ui/AtlantaColors.kt
+++ core/src/commonMain/kotlin/ui/AtlantaColors.kt
@@ -4,11 +4,19 @@ import androidx.compose.ui.graphics.Color
 
 object AtlantaColors {
     val Gray = Color(0xFF838383)
+    val Telegram = Color(0xFF00AAFF)
+    val GrayForNavBar = Color(0xFFF9F9F9)
+    val Gray0 = Color(0xFFF2F2F2)
+    val Gray2 = Color(0xFFCDCDCD)
+    val Gray3 = Color(0xFF818C9C)
     val DarkGray = Color(0xFF161616)
     val Dark = Color(0xFF222222)
     val LightGray = Color(0xFFB2B2B2)
+    val LightGray2 = Color(0xFFE6E6E6)
+    val LightGray3 = Color(0xFFF1F1F1)
     val DarkGray2 = Color(0xFF333333)
     val LightGrayForSwipe = Color(0xFFEFEFEF)
     val DarkGrayForSwipe = Color(0xFF7A7A7A)
     val Blue = Color(0xFF007AFF)
+    val LightBlue = Color(0xFFD9E5F6)
 }

df39b3892baf7958c2547f83f9d630e426a0d89a
PolyakovMA <polyakovma@tis-online.com>
2025-12-12 02:10:12 +0500

    Create android text

--- core/src/androidMain/kotlin/utils/AtlantaText.android.kt
+++ core/src/androidMain/kotlin/utils/AtlantaText.android.kt
@@ -2,7 +2,30 @@ package utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 actual fun AtlantaText(text: String, weight: Float, size: Float, color: Color) {
+    androidx.compose.material3.Text(
+        text = text,
+        style = TextStyle(
+            fontFamily = FontFamily.Default, //  ROBOTO
+            fontWeight = mapWeight(weight),
+            fontSize = size.sp,
+            letterSpacing = 0.sp
+        ),
+        color = color
+    )
 }
+
+fun mapWeight(weight: Float): FontWeight =
+    when {
+        weight <= 300f -> FontWeight.Light
+        weight <= 400f -> FontWeight.Normal
+        weight <= 500f -> FontWeight.Medium
+        weight <= 600f -> FontWeight.SemiBold
+        else -> FontWeight.Bold
+    }
